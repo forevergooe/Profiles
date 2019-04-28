7ff045445f4581ed83366730c3ca4797422c415c
Conners Hua <ConnersHua@iCloud.com>
2019-04-28 22:09:17 +0800

    feature：测试 Scripting

---
+++ Surge/Scripting/WeChat.js
@@ -0,0 +1,12 @@
+let result = body;
+
+// Subscriptions articles
+let subscriptionsArticles = '/mp/getappmsgad?f=';
+
+if (url.indexOf(subscriptionsArticles) != -1) {
+    var jsbody = JSON.parse(body);
+    jsbody.advertisement_info = [];
+    result = JSON.stringify(jsbody);
+}
+
+result;
